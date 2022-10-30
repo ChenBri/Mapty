31a9cf4b3ae0655ece17a4f23860b9a38d294c08
Chen Brilling <chenbrilling@gmail.com>
2022-10-30 18:13:08 +0200

    Created google maps URL from current location

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+
+navigator.geolocation.getCurrentPosition(function(location){
+    const lat = location.coords.latitude;
+    const lon = location.coords.longitude;
+    const url = `https://www.google.com/maps/@${lat},${lon},15z`;
+    console.log(url)
+}, function(){
+    alert('Failed to get current location')
+})
